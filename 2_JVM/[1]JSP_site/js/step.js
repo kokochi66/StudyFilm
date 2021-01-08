0796428ee85de5043955a8b1184a145f049dc13d
kokochi66 <kokochikochi@gmail.com>
2021-01-09 02:02:43 +0900

    2020.01.08.JSP

--- 2_JVM/[1]JSP_site/js/step.js
+++ 2_JVM/[1]JSP_site/js/step.js
@@ -1,5 +1,7 @@
 
 const menuSide_step = document.querySelector("#side-step");
+const cont_list = document.querySelector("#contents .col1 .contents_style ul li");
+
 menuIcon.addEventListener("click", function() {
   if(onMenu) menuSide_step.classList.add("active");
   else menuSide_step.classList.remove("active");
@@ -9,4 +11,15 @@ menuSide_step.addEventListener("mouseenter", () => {
 });
 menuSide_step.addEventListener("mouseleave", () => {
   cursor.classList.remove("active");
+});
+
+document.querySelectorAll("#contents .col .contents_style ul li").forEach(elem => {
+  elem.addEventListener("mouseenter", () => {
+    cursor.style.opacity = 0;
+    elem.classList.add("active");
+  });
+  elem.addEventListener("mouseleave", () => {
+    cursor.style.removeProperty("opacity");
+    elem.classList.remove("active");
+  });
 });
