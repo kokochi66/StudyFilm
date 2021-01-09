3a8c3c0ef22a2d8435d318ea374aac5b7c0ffecd
kokochi66 <kokochikochi@gmail.com>
2021-01-10 06:06:21 +0900

    2020.01.09.JSP

--- 2_JVM/[1]JSP_site/js/main.js
+++ 2_JVM/[1]JSP_site/js/main.js
@@ -6,6 +6,8 @@ const menuItem = document.querySelector(".side-contents");
 const menuSide_Cont = document.querySelector("#side-cont");
 const menuTitle = document.querySelector("#title");
 var onMenu = false;
+
+// 메뉴 아이콘 클릭 효과
 menuIcon.addEventListener("click", function() {
   if(onMenu) {
     this.classList.remove("active");
@@ -19,21 +21,29 @@ menuIcon.addEventListener("click", function() {
   }
   onMenu = !onMenu;
 });
+
+// 커서이동
 document.addEventListener("mousemove", function(e){
   gsap.to(cursor, {duration: .1, left: e.clientX -10, top: e.clientY });
 });
+
+// 메뉴 아이콘 마우스 오버효과
 menuIcon.addEventListener("mouseenter", () => {
   cursor.classList.add("active");
 });
 menuIcon.addEventListener("mouseleave", () => {
   cursor.classList.remove("active");
 });
+
+// 메뉴 타이틀 마우스 오버효과
 menuTitle.addEventListener("mouseenter", () => {
   cursor.classList.add("active");
 });
 menuTitle.addEventListener("mouseleave", () => {
   cursor.classList.remove("active");
 });
+
+// 메뉴 사이드 마우스 오버효과
 document.querySelectorAll(".side-contents a").forEach(elem => {
   elem.addEventListener("mouseenter", () => {
     cursor.classList.add("active");
@@ -43,13 +53,14 @@ document.querySelectorAll(".side-contents a").forEach(elem => {
   });
 });
 
+// 스크롤시 메뉴의 색 변경효과
 window.addEventListener('scroll', function(e){
   if(window.scrollY == 0) {
       menu.classList.remove("scroll");
       menuTitle.classList.remove("scroll");
       menuIcon.classList.remove("scroll");
   }
-  else {
+  else if(!onPage) {
     menu.classList.add("scroll");
     menuTitle.classList.add("scroll");
     menuIcon.classList.add("scroll");
--- 2_JVM/[1]JSP_site/js/step.js
+++ 2_JVM/[1]JSP_site/js/step.js
@@ -1,18 +1,28 @@
 
 const menuSide_step = document.querySelector("#side-step");
 const cont_list = document.querySelector("#contents .col1 .contents_style ul li");
+var onPage = false;
 
+// 메뉴 아이콘 클릭효과 (메인에 추가)
 menuIcon.addEventListener("click", function() {
-  if(onMenu) menuSide_step.classList.add("active");
-  else menuSide_step.classList.remove("active");
+  if(onMenu) {
+    menuSide_step.classList.add("active");
+    $("#pIcon").css('z-index',50);
+  }
+  else {
+    menuSide_step.classList.remove("active");
+    $("#pIcon").css('z-index',102);
+  }
 });
+
+// 사이드 마우스 오버효과
 menuSide_step.addEventListener("mouseenter", () => {
   cursor.classList.add("active");
 });
 menuSide_step.addEventListener("mouseleave", () => {
   cursor.classList.remove("active");
 });
-
+// 게시판 마우스 오버효과
 document.querySelectorAll("#contents .col .contents_style ul li").forEach(elem => {
   elem.addEventListener("mouseenter", () => {
     cursor.style.opacity = 0;
@@ -23,3 +33,79 @@ document.querySelectorAll("#contents .col .contents_style ul li").forEach(elem =
     elem.classList.remove("active");
   });
 });
+// 페이지 아이콘 오버효과
+$("#icon").hover(function(){
+  cursor.classList.add("active");
+}, function() {
+  cursor.classList.remove("active");
+});
+
+// JDK 설치방법 문서열기
+$(".page_btn").click(function(){
+  $(this).parent().parent().siblings(".page").addClass("active");
+  $("#pIcon").addClass("active");
+  menu.classList.remove("scroll");
+  menuTitle.classList.remove("scroll");
+  menuIcon.classList.remove("scroll");
+  onPage = true;
+});
+$("#pIcon #icon").click(function(){
+  $(".page").removeClass("active");
+  $("#pIcon").removeClass("active");
+  menu.classList.add("scroll");
+  menuTitle.classList.add("scroll");
+  menuIcon.classList.add("scroll");
+  onPage = false;
+});
+
+// 마우스 휠 고정이벤트 (문서가 켜졌을 때)
+$("#wrap").on('scroll touchmove mousewheel', function(event) {
+  if(onPage) {
+    event.preventDefault();
+    event.stopPropagation();
+    return false;
+  }
+});
+
+// 목차 클릭 이동 이벤트
+var cont_li = [];
+var winHeight = $(window).height()/10;
+for(var i = 0; i < 5; i++) cont_li[i] = $("#contents .col").eq(i);
+$("#contents .col1 .contents_style ul li").click(function(e){
+  var index = $(this).index()+1;
+  var offset = cont_li[index].offset().top - winHeight;
+  $("html, body").animate({scrollTop:offset},550,"easeInOutExpo");
+  // console.log(cont_li[index].offset().top +" "+ index);
+})
+$("#side-step li").click(function(e){
+  var index = $(this).index();
+  var offset = 0;
+  if(index < 5) offset = cont_li[index].offset().top - winHeight;
+  $("html, body").animate({scrollTop:offset},550,"easeInOutExpo");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
