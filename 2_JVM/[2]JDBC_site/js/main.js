7c612b2c064be11e7d62a776f5217b3ef9610124
kokochi66 <kokochikochi@gmail.com>
2021-01-28 13:54:58 +0900

    2021.01.28.JDBC

--- 2_JVM/[2]JDBC_site/js/main.js
+++ 2_JVM/[2]JDBC_site/js/main.js
@@ -1,6 +1,23 @@
-var cont_text = document.querySelectorAll('#contents .step .text'),
+// 변수선언
+var side_btn = document.querySelector('.menuBtn'),
+    side = document.getElementById('side'),
+    cont_text = document.querySelectorAll('#contents .step .text'),
     cont_bar = document.querySelectorAll('#contents .step .bar');
 
+
+// 헤더 사이드 열기
+side_btn.addEventListener('click', () => {
+  if(side.classList.contains('active')) {
+    side.classList.remove('active');
+    side_btn.classList.remove('active');
+  }
+  else {
+    side.classList.add('active');
+    side_btn.classList.add('active');
+  }
+});
+
+// 메인컨텐츠 마우스 오버효과
 cont_text.forEach(elem => {
   elem.addEventListener('mouseenter', () => {
     elem.parentNode.classList.add('hover');
