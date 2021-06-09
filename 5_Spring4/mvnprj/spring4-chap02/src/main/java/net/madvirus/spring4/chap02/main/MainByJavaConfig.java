1768b9737b992c0269acfa6cdf0b7490b9341236
kokochi66 <kokochikochi@gmail.com>
2021-06-10 03:05:03 +0900

    2021.06.10 Spring4-chap02 Study

---
+++ 5_Spring4/mvnprj/spring4-chap02/src/main/java/net/madvirus/spring4/chap02/main/MainByJavaConfig.java
@@ -0,0 +1,22 @@
+package net.madvirus.spring4.chap02.main;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import net.madvirus.spring4.chap02.AuthenticationService;
+import net.madvirus.spring4.chap02.PasswordChangeService;
+import net.madvirus.spring4.chap02.conf.Config;
+
+public class MainByJavaConfig {
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext ctx = 
+				new AnnotationConfigApplicationContext(Config.class);
+		AuthenticationService authSvc = 
+				ctx.getBean("authenticationService", AuthenticationService.class);
+		authSvc.authenticate("bkchoi", "1234");
+		
+		PasswordChangeService pwChgSvc =
+				ctx.getBean(PasswordChangeService.class);
+		pwChgSvc.changePassword("bkchoi", "1234" , "5678");
+		ctx.close();
+	}
+}
