214ed5126898cf6a6f96f7894522941746c86c70
kokochi66 <61536109+kokochi66@users.noreply.github.com>
2021-06-25 22:28:13 +0900

    2021.06.25 springprj

---
+++ 5_Spring4/hdSpring/com.kokochi.samp/src/main/java/com/kokochi/samp/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.kokochi.samp.controller;
+
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	
+	@RequestMapping(value="/")
+	public String home(Locale locale, Model model) { // 메인 home 화면 매핑
+		logger.info("/ - Home Mapping :: Locale = "+ locale);
+		
+		return "home";
+	}
+}
