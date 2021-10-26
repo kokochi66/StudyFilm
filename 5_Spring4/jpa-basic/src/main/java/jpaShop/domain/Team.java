60a7601de0e42974924625dbc170a9396c94aec6
kokoc <kokochikochi@gmail.com>
2021-10-27 06:44:19 +0900

    2021.10.27 JPA 단방향 연관관계와 양방향 연관관계

---
+++ 5_Spring4/jpa-basic/src/main/java/jpaShop/domain/Team.java
@@ -0,0 +1,43 @@
+package jpaShop.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Team {
+    @Id @GeneratedValue
+    private Long id;
+    private String  name;
+
+    // 양방향 연관관계
+    @OneToMany(mappedBy = "team")      // 반대편에 걸려있는 변수값을 mappedBy를 통해서 지정해준다.
+    private List<Member> members = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
+    }
+}
--- 5_Spring4/jpa-basic/src/main/java/jpaShop/main/JpaMain.java
+++ 5_Spring4/jpa-basic/src/main/java/jpaShop/main/JpaMain.java
@@ -1,9 +1,13 @@
 package jpaShop.main;
 
+import jpaShop.domain.Member;
+import jpaShop.domain.Team;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -14,6 +18,24 @@ public class JpaMain {
         transaction.begin();
 
         try {
+
+            Team team = new Team();
+            team.setName("TeamA");
+            entityManager.persist(team);
+
+            Member member = new Member();
+            member.setName("member1");
+            member.changeTeam(team);
+            entityManager.persist(member);
+
+//            entityManager.flush();
+//            entityManager.clear();
+
+            Team team1 = entityManager.find(Team.class, team.getId());
+            List<Member> members = team1.getMembers();
+            for (Member member1 : members) {
+                System.out.println("TEST :: members :: " + member1.getId()+" " + member1.getName());
+            }
 
             transaction.commit();
         } catch (Exception e) {
