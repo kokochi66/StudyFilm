6a3fbfc34b5f3c6a5cdc26056949bbd3a916de88
kokochi66 <61536109+kokochi66@users.noreply.github.com>
2022-09-14 09:26:49 +0900

    jpashop 2일차

--- inflearn/inflearnSpringJPA1/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ inflearn/inflearnSpringJPA1/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -9,6 +9,9 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.CascadeType.*;
+import static javax.persistence.FetchType.*;
+
 @Entity
 @Table(name = "orders")
 @Getter @Setter
@@ -18,14 +21,14 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = LAZY)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -34,4 +37,20 @@ public class Order {
     @Enumerated(EnumType.STRING)
     @Column(name = "order_status")
     private OrderStatus status; // 주문상태
+
+    // 연관관계 메소드 => 양방향일 때, 양방향 모두에 세팅될 수 있도록 설정
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
