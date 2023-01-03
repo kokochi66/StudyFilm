9453989f8f055092b43cfebef5aae1f42439041b
kokochi66 <61536109+kokochi66@users.noreply.github.com>
2023-01-03 21:30:15 +0900

    API 추가

---
+++ inflearn/inflearnSpringJPA1/jpashop/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,90 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.order.Order;
+import jpabook.jpashop.domain.order.OrderItem;
+import jpabook.jpashop.domain.order.OrderSearch;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> list = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : list) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(orderItem -> orderItem.getItem().getName());
+        }
+        return list;
+    }
+
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<OrderDto> res = orders.stream().map(OrderDto::new).collect(Collectors.toList());
+        return res;
+    }
+
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderDto> res = orders.stream().map(OrderDto::new).collect(Collectors.toList());
+        return res;
+    }
+
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> ordersV3_1_page(@RequestParam(value = "offset", defaultValue = "0")  int offset,
+                                          @RequestParam(value = "limit", defaultValue = "100") int limit) {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+        List<OrderDto> res = orders.stream().map(OrderDto::new).collect(Collectors.toList());
+        return res;
+    }
+
+    @Getter
+    @Setter
+    static class OrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order order) {
+            this.orderId = order.getId();
+            this.name = order.getMember().getName();
+            this.orderDate = order.getOrderDate();
+            this.address = order.getDelivery().getAddress();
+            this.orderItems = order.getOrderItems().stream().map(OrderItemDto::new).collect(Collectors.toList());
+        }
+    }
+
+    @Getter
+    @Setter
+    static class OrderItemDto {
+        private String itemName;
+        private int orderPrice;
+        private int count;
+
+        public OrderItemDto(OrderItem orderItem) {
+            this.itemName = orderItem.getItem().getName();
+            this.orderPrice = orderItem.getOrderPrice();
+            this.count = orderItem.getCount();
+        }
+    }
+}
--- inflearn/inflearnSpringJPA1/jpashop/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ inflearn/inflearnSpringJPA1/jpashop/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -84,4 +84,36 @@ public class OrderRepository {
                         " join o.delivery d", SimpleOrderQueryDto.class
         ).getResultList();
     }
+
+    public List<Order> findAllWithItem() {
+        return em.createQuery(
+                "select distinct o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d" +
+                        " join fetch o.orderItems oi" +
+                        " join fetch oi.item i", Order.class
+        ).getResultList();
+    }
+
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
+    // 일대다 매핑의 경우의 페치 조인과 페이징
+    // 일대다 매핑의 경우 distinct를 사용해서 중복되는 데이터를 자동으로 제거해주는 기능을 JPA 자체적으로 지원해준다.
+    // 그러니 distinct를 사용하면 일단 쿼리문에서 모든 데이터를 다 조회를 한 뒤에, 조회한 데이터에서 메모리 내부에서 페이징을 처리하기 때문에, 메모리가 터질 위험이 있다.
+    // 딱 필요한 데이터만 가져올 수 있는 Dto로 조회하는 방법을 선택해야 한다.
+
+    // 페치조인의 경우, ToOne관계의 엔티티는 페치조인이 아무리 많아져도 상관없다.
+    // ToMany관계의 엔티티를 페치조인할 때에는 많은 고민이 필요하며, 두개 이상은 적용해서는 안된다.
+
+
+
+
 }
