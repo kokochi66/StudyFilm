d1830fe52d5ca432574c3ceb0548ddcb98684aaa
kokochi66 <kokochikochi@gmail.com>
2023-01-16 18:57:45 +0900

    JPA 쿼리 정의, DTO 조회 ,파라미터 바인딩, 반환타입 학습

--- inflearn/springDataJpa/data-jpa/src/main/java/Study/datajpa/repository/MemberRepository.java
+++ inflearn/springDataJpa/data-jpa/src/main/java/Study/datajpa/repository/MemberRepository.java
@@ -1,11 +1,13 @@
 package Study.datajpa.repository;
 
 import Study.datajpa.entity.Member;
+import Study.datajpa.entity.MemberDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
 
 @Repository
@@ -17,4 +19,18 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
 //    @Query(name = "Member.findByName") NamedQuery는 이게 없어도 알아서 우선순위가 먼저 찾아서 안해줘도 된다.
     List<Member> findByUserName(@Param("name") String name);
+
+    @Query("select m from Member m where m.name = :name and m.age = :age")
+    List<Member> findUser(@Param("name") String name, @Param("age") int age);
+
+    @Query("select m.name from Member m")
+    List<String> findUsernameList();
+
+    @Query("select new Study.datajpa.entity.MemberDto(m.id, m.name, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
+
+    @Query("select m from Member m where m.name in :names")
+    List<Member> findByNames(@Param("names") Collection<String> names);
+
+    Member findAllByName(String name);
 }
--- inflearn/springDataJpa/data-jpa/src/test/java/Study/datajpa/repository/MemberRepositoryTest.java
+++ inflearn/springDataJpa/data-jpa/src/test/java/Study/datajpa/repository/MemberRepositoryTest.java
@@ -1,12 +1,15 @@
 package Study.datajpa.repository;
 
 import Study.datajpa.entity.Member;
+import Study.datajpa.entity.MemberDto;
+import Study.datajpa.entity.Team;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -84,5 +87,114 @@ class MemberRepositoryTest {
         for (Member member : result) {
             System.out.println("TEST :: member = " + member);
         }
+    }
+
+    @Test
+    public void findTestNamedQuery2() {
+        Member member1 = Member.createMember("kokochi1", 20);
+        Member member2 = Member.createMember("kokochi2", 27);
+        Member member3 = Member.createMember("kokochi3", 27);
+        Member member4 = Member.createMember("kokochi4", 27);
+        Member member5 = Member.createMember("kokochi5", 27);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        memberRepository.save(member3);
+        memberRepository.save(member4);
+        memberRepository.save(member5);
+
+        List<Member> result = memberRepository.findUser("kokochi1", 20);
+
+        for (Member member : result) {
+            System.out.println("TEST :: member = " + member);
+        }
+    }
+
+    @Test
+    public void findTestUserName() {
+        Member member1 = Member.createMember("kokochi1", 20);
+        Member member2 = Member.createMember("kokochi2", 27);
+        Member member3 = Member.createMember("kokochi3", 27);
+        Member member4 = Member.createMember("kokochi4", 27);
+        Member member5 = Member.createMember("kokochi5", 27);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        memberRepository.save(member3);
+        memberRepository.save(member4);
+        memberRepository.save(member5);
+
+        List<String> result = memberRepository.findUsernameList();
+        for (String res : result) {
+            System.out.println("TEST :: " + res);
+        }
+    }
+
+    @Autowired
+    private TeamRepository teamRepository;
+
+    @Test
+    public void findTestMemberDto() {
+        Team teamA = Team.createTeam("teamA");
+        Team teamB = Team.createTeam("teamB");
+        Team teamC = Team.createTeam("teamC");
+
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+        teamRepository.save(teamC);
+
+        Member member1 = Member.createMember("kokochi1", 20, teamA);
+        Member member2 = Member.createMember("kokochi2", 27, teamA);
+        Member member3 = Member.createMember("kokochi3", 27, teamB);
+        Member member4 = Member.createMember("kokochi4", 27, teamB);
+        Member member5 = Member.createMember("kokochi5", 27, teamC);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        memberRepository.save(member3);
+        memberRepository.save(member4);
+        memberRepository.save(member5);
+
+        List<MemberDto> memberDto = memberRepository.findMemberDto();
+        for (MemberDto dto : memberDto) {
+            System.out.println("TEST :: dto = " + dto);
+        }
+    }
+
+    @Test
+    public void findByNamesForParameterBinding() {
+
+        Member member1 = Member.createMember("kokochi1", 20);
+        Member member2 = Member.createMember("kokochi2", 27);
+        Member member3 = Member.createMember("kokochi3", 31);
+        Member member4 = Member.createMember("kokochi4", 55);
+        Member member5 = Member.createMember("kokochi5", 80);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        memberRepository.save(member3);
+        memberRepository.save(member4);
+        memberRepository.save(member5);
+
+        List<Member> byNames = memberRepository.findByNames(Arrays.asList("kokochi1", "kokochi2"));
+        for (Member byName : byNames) {
+            System.out.println("TEST :: name = " + byName);
+        }
+    }
+
+    @Test
+    public void findAllByNameOne() {
+
+        Member member1 = Member.createMember("kokochi", 20);
+        Member member2 = Member.createMember("kokochi", 27);
+        Member member3 = Member.createMember("kokochi", 31);
+        Member member4 = Member.createMember("kokochi", 55);
+        Member member5 = Member.createMember("kokochi", 80);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        memberRepository.save(member3);
+        memberRepository.save(member4);
+        memberRepository.save(member5);
+
+        Member kokochi = memberRepository.findAllByName("kokochi");
+        System.out.println("TEST :: kokochi = " + kokochi);
+        // 여러개가 나오는 쿼리문인데 단일 리턴타입을 지정하면 IncorrectResultSizeDataAccessException이 발생한다.
+        // 반드시 유니크한 조건에만 단건조회로 설정해야 한다.
     }
 }
