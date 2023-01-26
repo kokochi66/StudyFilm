1da0e57ed71358214e947a89fff5373b3471bada
kokochi66 <61536109+kokochi66@users.noreply.github.com>
2023-01-26 20:36:43 +0900

    Repository 수정

--- inflearn/inflearnSpringJPA1/jpashop/jpashop/src/main/java/jpabook/jpashop/repository/SpringDataMemberRepository.java
+++ inflearn/inflearnSpringJPA1/jpashop/jpashop/src/main/java/jpabook/jpashop/repository/SpringDataMemberRepository.java
@@ -1,11 +1,12 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface SpringDataMemberRepository extends org.springframework.data.repository.Repository<Member, Long> {
+public interface SpringDataMemberRepository extends JpaRepository<Member, Long> {
     List<Member> findAllByName(String name);
 }
