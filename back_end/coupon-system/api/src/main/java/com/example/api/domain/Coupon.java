660b9736592af55189bdfe7381915d3cb71ac81c
kokochi66 <61536109+kokochi66@users.noreply.github.com>
2023-11-21 08:56:30 +0900

    coupon test

---
+++ back_end/coupon-system/api/src/main/java/com/example/api/domain/Coupon.java
@@ -0,0 +1,28 @@
+package com.example.api.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Coupon {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+
+    public Coupon(Long userId) {
+        this.userId = userId;
+    }
+}
