d37af3414d36f6a119919a403cd0d1e37a05eafe
kokochi66 <61536109+kokochi66@users.noreply.github.com>
2024-04-20 18:39:54 +0900

    스터디용 프로젝트 생성

---
+++ back_end/thymeleaf-basic/src/main/java/hello/thymeleaf/ThymeleafBasicApplication.java
@@ -0,0 +1,14 @@
+package hello.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.validation.BindingResult;
+
+@SpringBootApplication
+public class ThymeleafBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafBasicApplication.class, args);
+	}
+
+}
